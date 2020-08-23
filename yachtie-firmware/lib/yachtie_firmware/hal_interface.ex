74204789390cf6db9891c134e75fb4bf60644edc
Josh McArthur <joshua.mcarthur@gmail.com>
2020-08-23 18:04:11 +1200

    Add a GenServer capable of basic communication over serial with HAL

---
+++ yachtie-firmware/lib/yachtie_firmware/hal_interface.ex
@@ -0,0 +1,38 @@
+defmodule Yachtie.Firmware.HALInterface do
+  use GenServer
+  alias Circuits.UART
+  alias Yachtie.PubSub
+  require Logger
+
+  def start_link(state \\ %{}) do
+    state = state |> Map.put_new(:port, "ttyAMA0")
+                  |> Map.put_new(:speed, 115200)
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
+
+  def init(state) do
+    send self(), :init
+    {:ok, state}
+  end
+
+  def handle_info(:init, %{port: port, speed: speed} = state) do
+    {:ok, uart} = UART.start_link()
+    :ok = UART.open(uart, port, speed: speed, framing: {Circuits.UART.Framing.Line, separator: "\n"})
+    {:noreply, state |> Map.put(:uart, uart)}
+  end
+
+  def handle_info({:circuits_uart, name, message}, %{port: port} = state) when name == port do
+    Logger.info(message)
+    {:noreply, state}
+  end
+  
+  def handle_info(_msg, state) do
+    {:noreply, state}
+  end
+
+  def handle_cast({:command, %{type: key, value: value}}, %{uart: uart} = state) do
+    {:ok, raw_message} = Jason.encode(%{type: key, value: value})
+    :ok = UART.write(uart, raw_message)
+    {:noreply, state}
+  end
+end
